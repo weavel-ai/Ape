3f56104f94b0b5ffa09c410da1db25356abb2864
engineerA314 <engineerA314@github.com>
2024-09-01 17:06:09 -0700

    UPDATE: update version to 0.7.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ape-core",
-    version="0.7.0",
+    version="0.7.1",
     packages=find_namespace_packages(),
     include_package_data=True,
     entry_points={},
